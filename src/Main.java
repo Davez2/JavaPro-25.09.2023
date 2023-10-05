73ef3021ffd371c977d276672262ac301716caa9
Davez2 <babkovdavid97@gmail.com>
2023-10-05 07:59:44 +0300

    lesson3 homework commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dog dog = new Dog();
+        dog.run(400);
+        dog.swim(11);
+        Cat cat = new Cat();
+        cat.run(200);
+        cat.swim(5);
+
+        System.out.println("Animals: " + Animal.getCount());
+        System.out.println("Dogs: " + Dog.getCount());
+        System.out.println("Cats: " + Cat.getCount());
     }
 }
