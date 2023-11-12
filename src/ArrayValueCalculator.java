5cf2902d26955462e6831c903c4df3bd27125270
Davez2 <babkovdavid97@gmail.com>
2023-11-12 11:18:55 +0200

    lesson12 homework commit

---
+++ src/ArrayValueCalculator.java
@@ -0,0 +1,19 @@
+public class ArrayValueCalculator {
+    public int doCalc(String[][] array) throws ArraySizeException, ArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new ArraySizeException("Array size must be 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new ArrayDataException("Invalid data at cell [" + i + "][" + j + "]", e);
+                }
+            }
+        }
+        return sum;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] array = {{"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}};
+
+        ArrayValueCalculator calc = new ArrayValueCalculator();
+        try {
+            int sum = calc.doCalc(array);
+            System.out.println("Sum: " + sum);
+        } catch (ArraySizeException | ArrayDataException e) {
+            e.printStackTrace();
+        }
     }
 }
